9471c6a960e8f231d2d242c925ebbea8cc359739
jmeegan2 <jmeegan8@gmail.com>
2022-09-13 18:15:08 -0400

    mostly notes

--- src/basics.java
+++ src/basics.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class basics {
+public class basics {
+    public static void main(String[] args)
+    }
 }
+/*
+Overall features
+1. strongly typed
+2. compile once, run anywhere
+3. top-level object class
+4. all objects are references (to an object instance)
+5. garbage collection
+6. single inheritance
+7. multi-threaded
+8. exception handling
+
+Strongly typed
+* Every variable must be declared with a data type and it must stay as that type
+
+Classes
+* Implementation of an abstract data type, cannot access the class unless its declared as public instead of private.
+    regular classes are declared public. There are other classes (e.g., inner classes) that are private.
+
+Data type
+* A data type is a set of values and a set of operators that operate on those values
+
+for example, the integer (primitive data) type:
+(1) set of integer value
+
+Abstract Data type
+* Programmer defined type is a new data type constructed
+
+Common methods of a class
+* Constructors
+* Getters
+* Operations
+
+Encapsulation
+* Provides a form of data hiding in OOP, in which class members can be hidden (declared private) and other made
+    accessible (declared public)
+* Instance variables are declared private, and methods declared public
+
+Special Method toString
+* Inherited from top-level object class. Produces an object's value as string
+    .toString
+
+Static members of a class
+* There is only one instance of a static class for all objects of that class
+
+
+
+ */
